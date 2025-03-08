fe9050dee4732e699f32ec0c77316912d78c7381
Steve Bandy <sbandyman@gmail.com>
2025-03-08 16:02:43 -0500

    spin more during auto

--- src/main/java/frc/robot/commands/SpitCoralAutonCommand.java
+++ src/main/java/frc/robot/commands/SpitCoralAutonCommand.java
@@ -8,7 +8,7 @@ import frc.robot.subsystems.PivotArmSubsystem;
 public class SpitCoralAutonCommand extends Command{
     
     private double startingRotations;
-    private static final double endRotations = Centimeters.of(30).div(Centimeters.of(6).times(Math.PI)).magnitude() + 2;
+    private static final double endRotations = Centimeters.of(30).div(Centimeters.of(6).times(Math.PI)).magnitude() + 8;
     private PivotArmSubsystem pivotArmSubsystem;
     
     public SpitCoralAutonCommand(PivotArmSubsystem subsystem){
